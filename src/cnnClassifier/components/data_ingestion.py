aa57a4b3e61c481456e61c32a44c8a3662aba4f0
Ashish <machinelearning.mnnlr.ashish@gmail.com>
2025-01-16 19:41:07 +0530

    model training

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -5,12 +5,10 @@ from cnnClassifier import logger
 from cnnClassifier.utils.common import get_size
 from cnnClassifier.entity.config_entity import DataIngestionConfig
 
-
 class DataIngestion:
     def __init__(self, config: DataIngestionConfig):
         self.config = config
 
-    
     def download_file(self)-> str:
         '''
         Fetch data from the url
